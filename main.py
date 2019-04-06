8e4b34031223d470d54c5afb2a972abfa7492534
lziad <23732689+lziad@users.noreply.github.com>
2019-04-06 17:23:46 +0800

    increase robustness

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import os
+import sys
 import json
+import time
 import sched
 import logging
 import threading
 from time import time, sleep
 from challenge import Challenge
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import ChatMember, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, MessageHandler, CallbackQueryHandler, Filters
-from telegram.error import TelegramError
+from telegram.error import TelegramError, BadRequest, TimedOut
 
 logging.basicConfig(level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -14,10 +17,20 @@ logging.basicConfig(level=logging.INFO,
 config, config_lock = dict(), threading.Lock()
 updater = None
 dispatcher = None
-# Key: chat_id + '|' + user_id + '|' + msg_id
-# Value: (challenge object, event object)
+# Key: chat_id + '|' + msg_id
+# Value: (challenge object, int (target user id), event object)
 current_challenges, cch_lock = dict(), threading.Lock()
+# Key: chat_id + '|' + user_id
+# Value: Telegram ChatMember object
+user_previous_restrictions, upr_lock = dict(), threading.Lock()
 challenge_sched = sched.scheduler(time, sleep)
+
+
+def save_delete_message(bot, *args, **kwarg):
+    try:
+        bot.delete_message(*args, **kwarg)
+    except BadRequest:  # msg to delete not found
+        pass
 
 
 def load_config():
@@ -52,6 +65,13 @@ def challenge_user(bot, update):
             config_lock.release()
         return None
 
+    # get previous restriction data
+    member = bot.get_chat_member(msg.chat.id, msg.from_user.id)
+    upr_lock.acquire()
+    user_previous_restrictions['{chat}|{user}'.format(
+        chat=msg.chat.id, user=msg.from_user.id)] = member
+    upr_lock.release()
+
     # Attempt to restrict the user
     try:
         bot.restrict_chat_member(msg.chat.id, msg.from_user.id)
@@ -76,11 +96,17 @@ def challenge_user(bot, update):
 
     timeout = group_config['challenge_timeout']
 
-    bot_msg = bot.send_message(chat_id=msg.chat.id,
-        text=group_config['msg_challenge'].format(
-            timeout=timeout, challenge=challenge.qus()),
-        reply_to_message_id=msg.message_id,
-        reply_markup=InlineKeyboardMarkup(challenge_to_buttons(challenge)))
+    try:
+        bot_msg = bot.send_message(chat_id=msg.chat.id,
+            text=group_config['msg_challenge'].format(
+                timeout=timeout, challenge=challenge.qus()),
+            reply_to_message_id=msg.message_id,
+            reply_markup=InlineKeyboardMarkup(challenge_to_buttons(challenge)))
+    except BadRequest:  # msg to reply not found
+        bot_msg = bot.send_message(chat_id=msg.chat.id,
+            text=group_config['msg_challenge'].format(
+                timeout=timeout, challenge=challenge.qus()),
+            reply_markup=InlineKeyboardMarkup(challenge_to_buttons(challenge)))
 
     timeout_event = challenge_sched.enter(group_config['challenge_timeout'],
         10, handle_challenge_timeout,
@@ -120,7 +146,38 @@ def handle_challenge_timeout(bot, chat, user, bot_msg):
 
     if group_config['delete_failed_challenge']:
         challenge_sched.enter(group_config['delete_failed_challenge_interval'],
-            1, bot.delete_message, argument=(chat, bot_msg))
+            1, save_delete_message, argument=(bot, chat, bot_msg))
+
+
+def lift_restriction(bot, chat, target):
+    # restore the restriction to what its original state
+    upr_lock.acquire()
+    member = user_previous_restrictions.get('{chat}|{user}'.format(chat=chat, user=target))
+    upr_lock.release()
+    if not member:
+        member = ChatMember(None, '', until_date=None, 
+            can_send_messages=True, can_send_media_messages=True,
+            can_send_other_messages=True, can_add_web_page_previews=True)
+    else:
+        upr_lock.acquire()
+        del user_previous_restrictions['{chat}|{user}'.format(chat=chat, user=target)]
+        upr_lock.release()
+    # if until_date - now < 30 seconds the restriction would be infinite
+    # use 35 here for safety
+    if member.until_date and member.status == 'restricted' and member.until_date < time.time()+35:
+        member.can_send_messages = member.can_send_media_messages = True
+        member.can_send_other_messages = menber.can_add_web_page_previews = True
+        member.until_date = None
+    print(member.can_send_media_messages == False)
+    print(type(member.can_send_media_messages))
+    try:
+        bot.restrict_chat_member(chat, target, until_date=member.until_date,
+            can_send_messages=member.can_send_messages,
+            can_send_media_messages=member.can_send_media_messages,
+            can_send_other_messages=member.can_send_other_messages,
+            can_add_web_page_previews=member.can_add_web_page_previews)
+    except TelegramError as e:
+        raise e
 
 
 def handle_challenge_response(bot, update):
@@ -150,22 +207,24 @@ def handle_challenge_response(bot, update):
         ch_id = '{chat}|{msg}'.format(chat=chat, msg=bot_msg)
         cch_lock.acquire()
         challenge, target, timeout_event = current_challenges.get(ch_id, (None, None, None))
-        del current_challenges[ch_id]
+        try:
+            del current_challenges[ch_id]
+        except KeyError:
+            return None
         cch_lock.release()
         challenge_sched.cancel(timeout_event)
 
         if query['data'] == '+':
-            # lift the restriction
             try:
-                bot.restrict_chat_member(chat, target,
-                    can_send_messages=True, can_send_media_messages=True,
-                    can_send_other_messages=True, can_add_web_page_previews=True)
+                lift_restriction(bot, chat, target)
             except TelegramError:
                 bot.answer_callback_query(callback_query_id=query['id'],
                     text=group_config['msg_bot_no_permission'])
-                return None
-            bot.edit_message_text(group_config['msg_approved'].format(user=username),
-                chat_id=chat, message_id=bot_msg, reply_mark=None)
+            try:
+                bot.edit_message_text(group_config['msg_approved'].format(user=username),
+                    chat_id=chat, message_id=bot_msg, reply_mark=None)
+            except BadRequest:   # message to edit not found
+                pass
         else:  # query['data'] == '-'
             try:
                 bot.kick_chat_member(chat, target)
@@ -173,13 +232,21 @@ def handle_challenge_response(bot, update):
                 bot.answer_callback_query(callback_query_id=query['id'],
                     text=group_config['msg_bot_no_permission'])
                 return None
-            bot.edit_message_text(group_config['msg_refused'].format(user=username),
-                chat_id=chat, message_id=bot_msg, reply_mark=None)
+            try:
+                bot.edit_message_text(group_config['msg_refused'].format(user=username),
+                    chat_id=chat, message_id=bot_msg, reply_mark=None)
+            except BadRequest:   # message to edit not found
+                pass
+
+            upr_lock.acquire()
+            member = user_previous_restrictions.get('{chat}|{user}'.format(chat=chat, user=target))
+            if member:
+                del user_previous_restrictions['{chat}|{user}'.format(chat=chat, user=target)]
+            upr_lock.release()
 
         bot.answer_callback_query(callback_query_id=query['id'])
 
         return None
-
 
     ch_id = '{chat}|{msg}'.format(chat=chat, msg=bot_msg)
     cch_lock.acquire()
@@ -197,11 +264,8 @@ def handle_challenge_response(bot, update):
     del current_challenges[ch_id]
     cch_lock.release()
 
-    # lift the restriction
     try:
-        bot.restrict_chat_member(chat, target,
-            can_send_messages=True, can_send_media_messages=True,
-            can_send_other_messages=True, can_add_web_page_previews=True)
+        lift_restriction(bot, chat, target)
     except TelegramError:
         # This my happen when the bot is deop-ed after the user join
         # and before the user click the button
@@ -218,14 +282,14 @@ def handle_challenge_response(bot, update):
 
     if group_config['delete_passed_challenge']:
         challenge_sched.enter(group_config['delete_passed_challenge_interval'],
-            5, bot.delete_message, argument=(chat, bot_msg))
+            5, save_delete_message, argument=(bot, chat, bot_msg))
 
 
 def main():
     global updater, dispatcher
 
     load_config()
-    updater = Updater(config['token'])
+    updater = Updater(config['token'], request_kwargs={'proxy_url': 'http://127.0.0.1:8001'})
     dispatcher = updater.dispatcher
 
     challenge_handler = MessageHandler(Filters.status_update.new_chat_members,
@@ -248,6 +312,11 @@ def main():
         except KeyboardInterrupt:
             save_config()
             exit(0)
+        except TimedOut:
+            save_config()
+            # try to restart
+            logging.getLogger().info('Caught a Telegram TimedOut error, restarting...')
+            os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
 
 
 if __name__ == '__main__':
