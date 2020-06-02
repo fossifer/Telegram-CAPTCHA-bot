d431f77a95eb8e923cf43e0e634e9876465025a0
Davidzdh <30859354+Guanchishan@users.noreply.github.com>
2020-06-03 01:59:48 +0800

    fix bug in zh-TW and add cdo

--- config.example.js
+++ config.example.js
@@ -47,7 +47,7 @@
     }
     // Change "zh_TW" to your group id with quotes
     "zh_TW": {
-        "msg_self_introduction": "大家好，感謝使用本機器人。\n\n我負責排除掉討厭的廣告機器人，賦予我群組管理管中的 Ban users 權限即可開始使用，移除權限即可停用。新用戶加入群組時我會暫時將其禁止發言，並出一道小學二年級難度的算術題目，列出幾個選項讓用戶選擇。因為我比較仁慈，所以用戶只要點選按鈕即可獲得解鎖，甚至不需要答對。\n\n我不會收集群組裡的訊息，程式的原始碼是公開的，如果您對我的功能不滿意，可以點選我的頭像檢視 bio 中的原始碼連結，修改並運行您自己的機器人。",
+        "msg_self_introduction": "大家好，感謝使用本機器人。\n\n我負責排除掉討厭的廣告機器人，賦予我群組管理中的 Ban users 權限即可開始使用，移除權限即可停用。新用戶加入群組時我會暫時將其禁止發言，並出一道小學二年級難度的算術題目，列出幾個選項讓用戶選擇。因為我比較仁慈，所以用戶只要點選按鈕即可獲得解鎖，甚至不需要答對。\n\n我不會收集群組裡的訊息，程式的原始碼是公開的，如果您對我的功能不滿意，可以點選我的頭像檢視 bio 中的原始碼連結，修改並運行您自己的機器人。",
         "msg_challenge_not_for_you": "這次驗證並不針對您",
         "msg_challenge": "您好，本群組開啟了驗證功能，請在 {timeout} 秒內點下面的按鈕回答這個問題：\n{challenge}\n\nHi! The group has enabled CAPTCHA, please click one of the buttons to answer the following question:\n{challenge}",
         "msg_challenge_passed": "您已通過驗證，歡迎加入本群組！\n如果仍然無法發言，請重啟 Telegram 客戶端。\n\nWelcome to this group! You have passed the CAPTCHA. If you cannot send messages, please restart the Telegram client.",
@@ -82,6 +82,29 @@
         "msg_bot_no_permission": "操作錯誤，可能係機器人唔夠權限",
         "msg_approved": "閣下已經俾管理員 {user} 人工通過，歡迎您嘅加入！\n\nWelcome to this group! You were approved by {user} manually.",
         "msg_refused": "呢個用戶已經被管理員 {user} 踢出本群。\n\nThe user has been removed from group by {user}.",
+        "challenge_strict_mode": false,
+        "challenge_timeout": 180,
+        "challenge_timeout_action": "restrict",
+        "delete_passed_challenge": true,
+        "delete_passed_challenge_interval": 15,
+        "delete_failed_challenge": false,
+        "delete_failed_challenge_interval": 180
+    },
+    // Change "yue" to your group id with quotes
+    "cdo": {
+        // tbd
+            "msg_self_introduction": "大家儂好。謝謝各儂選擇本机器儂。\n\n廣告機器儂盡討厭，仱我會負責共伊逐去。乞我群組管理裡勢其 Ban users 權限，我就會開始工作；以後除迨者權限，我就會停下。新其用戶裡來，我先儥讓伊講話，出蜀芘小學二年級水平其算學題，下底攖幾隻答案乞用戶揀。我哩比較好心，在伊揀底蜀隻我都讓伊過，無管伊有賺無賺。\n\n我伓收集羣裡勢其消息，源代碼是公開其。汝㑚儥滿意我其功能，會使篤我其頭像，看 bio 裡勢其源代碼鏈接，修改囇再運行汝自家其机器儂。",
+        "msg_challenge_not_for_you": "這回驗證共汝無干過",
+        "msg_challenge": "汝好，儂家開了驗證功能，請着 {timeout} 秒之內，篤下底其數字，完成驗證：\n{challenge}Nṳ̄ hō̤, nàng-gā kŭi lāu ngiêng-céng gŭng-nèng, chiāng diŏh {timeout} miēu cĭ-nó̤i, dók â-dā̤ gì só-cê, uòng-sìng ngiêng-céng:\n{challenge}\n\nHi! The group has enabled CAPTCHA, please click one of the buttons to answer the following question:\n{challenge}",
+        "msg_challenge_passed": "汝已經通過驗證，歡迎加裡！\n汝㑚故是無辦法講話，關去 Telegram 客戶端再開起就會使。\n\nNṳ̄ ī-gĭng tŭng-guó ngiêng-céng, huăng-ngìng gă-diē! \nNṳ̄ nâ gó-sê mò̤ bâing-huák gōng-uâ, guŏng kó̤ Telegram káik-hô-duăng cái kŭi kī cêu â̤-sāi.\n\nWelcome to this group! You have passed the CAPTCHA. If you cannot send messages, please restart the Telegram client.",
+        "msg_challenge_mercy_passed": "雖然汝答案有賺，儂家故是認爲汝正是儂無賺。歡迎加裡！\n汝㑚故是無辦法講話，關去 Telegram 客戶端再開起就會使。\n\nChŭi-iòng nṳ̄ dák-áng ô dâng, nàng-gă gó-sê nêng-ùi nṳ̄ ciáng-sê nè̤ng mò̤ dâng. Huăng-ngìng gă-diē! \nNṳ̄ nâ gó-sê mò̤ bâing-huák gōng-uâ, guŏng kó̤ Telegram káik-hô-duăng cái kŭi kī cêu â̤-sāi.\n\nWelcome to this group! You chose a wrong button but we still recognize you as a human.\nIf you cannot send messages, please restart the Telegram client.",
+        "msg_challenge_failed": "儥好意思，汝無通過驗證。卜證明汝真其是儂，請發私訊乞管理員。\n\nMâ̤ hō̤ é-sé̤ṳ, nṳ̄ mò̤ tŭng-guó ngiêng-céng. Buóh céng-mìng nṳ̄ cĭng gì sê nè̤ng, chiāng huák sṳ̆-séng ké̤ṳk guāng-lī-uòng.\n\nSorry but you failed the CAPTCHA. Please contact group admins for help.",
+        "msg_approve_manually": "讓伊裡來",
+        "msg_refuse_manually": "共伊逐出",
+        "msg_permission_denied": "汝伓是管理員",
+        "msg_bot_no_permission": "操作有賺，可能機器儂其權限無夠",
+        "msg_approved": "汝已經乞管理員 {user} 人工通過，歡迎加裡！\n\nNṳ̄ ī-gĭng ké̤ṳk guāng-lī-uòng {user} ìng-gĕ̤ng tŭng-guó, huăng-ngìng gă-diē!\n\nWelcome to this group! You were approved by {user} manually.",
+        "msg_refused": "這隻用戶已經乞管理員 {user} 逐出本羣。\n\nCī-ciáh ê̤ṳng-hô ī-gĭng ké̤ṳk guāng-lī-uòng {user} dṳ̆k chók buōng-gùng.\n\nThe user has been removed from group by {user}.",
         "challenge_strict_mode": false,
         "challenge_timeout": 180,
         "challenge_timeout_action": "restrict",
